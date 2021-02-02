fa8088c0ccec5be086c7c4bfa5407fbb7fb2bec1
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2021-02-02 14:25:24 -0300

    FetchApiBackend

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingBox = () => {
+  return (
+    <>
+      <i className="fa fa-spinner fa-spin"></i> Loading...
+    </>
+  );
+};
+
+export default LoadingBox;
